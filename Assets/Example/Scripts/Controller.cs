3d1922a84a0d3126ab351a65cc87e90d6aed8f9b
Kristina Tiskeviciute <ktiskeviciute@yahoo.no>
2020-02-18 02:01:39 -0800

    Initial Commit

---
+++ Assets/Example/Scripts/Controller.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using UnityEngine;
+using Tubular;
+
+namespace Example
+{
+    [System.Serializable]
+    struct InputControls
+    {
+        public KeyCode LeftKey;
+        public KeyCode RightKey;
+        public KeyCode ForwardKey;
+        public KeyCode JumpKey;
+        public KeyCode StartTubeKey;
+        public KeyCode CloseTubeKey;
+        public KeyCode ClearAllTubesKey;
+    }
+
+    [RequireComponent(typeof(Tube))]
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(3f, 10.0f)]
+        private float speed = 3f;
+
+        [SerializeField]
+        [Range(1f, 10.0f)]
+        private float turnSpeed = 1f;
+
+        [SerializeField]
+        private AnimationCurve jumpCurve;
+
+        [SerializeField]
+        private float jumpHeight = 3;
+
+        [SerializeField]
+        private float jumpDuration = 1.0f;
+
+        [SerializeField]
+        private float jumpForwardSpeedModifier = 2.0f;
+
+        [SerializeField]
+        private InputControls Controls = new InputControls
+        {
+            LeftKey = KeyCode.LeftArrow,
+            RightKey = KeyCode.RightArrow,
+            ForwardKey = KeyCode.UpArrow,
+            JumpKey = KeyCode.Space,
+            StartTubeKey = KeyCode.RightAlt,
+            CloseTubeKey = KeyCode.RightControl,
+            ClearAllTubesKey = KeyCode.Delete
+        };
+
+        private float StartY { get; set; } = 0f;
+
+        private Tube MyTube { get; set; }
+
+        private bool OnGround { get; set; } = true;
+        private int VerticalMovementDirection { get; set; } = 0;   //-1: Going Down, 1: Going Up 0: Not Moving Vertically
+
+
+        private void Awake()
+        {
+            if (MyTube == null)
+            {
+                MyTube = GetComponent<Tube>();
+            }
+        }
+
+        private void Start()
+        {
+            StartY = transform.position.y;
+            MyTube.StartTube();
+
+
+        }
+
+        void Update()
+        {
+            if (Input.GetKey(Controls.ForwardKey))
+            {
+                transform.position = transform.position + (transform.forward * speed * Time.smoothDeltaTime);
+            }
+
+            if (Input.GetKey(Controls.LeftKey))
+            {
+                transform.Rotate(new Vector3(0, -turnSpeed, 0), Space.Self);
+            }
+
+            if (Input.GetKey(Controls.RightKey))
+            {
+                transform.Rotate(new Vector3(0, turnSpeed, 0), Space.Self);
+            }
+
+            if (OnGround && (Input.GetKey(Controls.RightKey) || Input.GetKey(Controls.LeftKey)) && !Input.GetKey(Controls.ForwardKey))
+            {
+                transform.position = transform.position + (transform.forward * speed * Time.smoothDeltaTime);
+            }
+
+
+            if ((Input.GetKey(Controls.JumpKey)))
+            {
+                if (OnGround)
+                    Jump();
+            }
+
+            if (Input.GetKey(Controls.CloseTubeKey))
+            {
+                MyTube.CloseTube();
+            }
+
+            if (Input.GetKey(Controls.StartTubeKey))
+            {
+                MyTube.StartTube();
+            }
+
+            if (Input.GetKey(Controls.ClearAllTubesKey))
+            {
+                MyTube.ClearTubes();
+            }
+
+        }
+
+        public void Jump()
+        {
+            VerticalMovementDirection = 1;
+            OnGround = false;
+            StartCoroutine(CurveInterp(Vector3.zero, new Vector3(0, jumpHeight, 0), jumpDuration, jumpCurve));
+        }
+
+        private void Land()
+        {
+            VerticalMovementDirection = -1;
+            StartCoroutine(CurveInterp(Vector3.zero, new Vector3(0, -jumpHeight, 0), jumpDuration, jumpCurve));
+        }
+
+        private IEnumerator CurveInterp(Vector3 startPos, Vector3 endPos, float duration, AnimationCurve curve)
+        {
+            Vector3 prevPos = startPos;
+            Vector3 targetPos;
+            float timer = 0;
+            float curveFraction;
+            float forwardSpeed = 0;
+
+            while (timer <= duration)
+            {
+                curveFraction = curve.Evaluate(Mathf.Clamp01(timer / duration));
+                targetPos = Vector3.LerpUnclamped(startPos, endPos, curveFraction);
+                transform.Translate(targetPos - prevPos, Space.Self);
+                prevPos = targetPos;
+                timer += Time.smoothDeltaTime;
+                forwardSpeed = (VerticalMovementDirection == 1) ? Mathf.Lerp(speed, speed * jumpForwardSpeedModifier, timer / duration) : Mathf.Lerp(speed * jumpForwardSpeedModifier, speed, timer / duration);
+                transform.position = transform.position + (transform.forward * forwardSpeed * Time.smoothDeltaTime);
+                yield return null;
+            }
+
+            if (VerticalMovementDirection == -1) // Going Down
+            {
+                transform.position = new Vector3(transform.position.x, StartY, transform.position.z);
+                OnGround = true;
+            }
+
+
+            if (VerticalMovementDirection == 1) // Going Up
+            {
+                transform.position = new Vector3(transform.position.x, jumpHeight + StartY, transform.position.z);
+                VerticalMovementDirection = 0;
+                Land();
+                yield break;
+            }
+
+            VerticalMovementDirection = 0;
+            yield break;
+        }
+
+    }
+}
---
+++ Assets/Tubular/Tube.cs
@@ -0,0 +1,239 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Tubular
+{
+    /// <summary>
+    /// Tube will create a Tube :)
+    /// </summary>
+    public class Tube : MonoBehaviour
+    {
+
+        const int VERTS_PER_LOOP = 25;
+
+        [SerializeField]
+        private float segmentLength = 10;
+        private GameObject CurrentSegment { get; set; }
+        private float currentSegmentLength;
+
+        [SerializeField]
+        [Range(0.1f, 2.0f)]
+        private float distanceBetweenLoops = 0.1f;
+
+        [SerializeField]
+        private float tubeRadius = 0.5f;
+
+        [SerializeField]
+        private Material material;
+
+        private GameObject TubeFrontSphere { get; set; }       
+        
+        private GameObject CurrentTubeParent;
+
+        private List<GameObject> Tubes { get; set; } = new List<GameObject>();
+        public ReadOnlyCollection<GameObject> ReadOnlyTubes => Tubes.AsReadOnly(); 
+        
+        private Mesh Mesh { get; set; }
+        private Vector4[] LoopVerts { get; set; }
+
+        private List<Vector3> Verts { get; set; } = new List<Vector3>();
+        private List<int> Tris { get; set; } = new List<int>();
+        private List<Vector2> UVs { get; set; } = new List<Vector2>();
+
+        private int LoopCount { get; set; }
+        private Vector3 LastLoopPos { get; set; }
+        private Vector3 PrevPos { get; set; }
+        private Vector3 PrevVelocity { get; set; }
+
+        
+        private Vector3 VelocityVector => PrevPos == PlayerTransform.position ? PlayerTransform.forward : (PlayerTransform.position - PrevPos).normalized;
+        private Vector3 UpVector => Vector3.Cross(VelocityVector, transform.right);
+        private Matrix4x4 LoopOrientMatrix => Matrix4x4.TRS(PrevPos, Quaternion.LookRotation(VelocityVector, -UpVector), Vector3.one);
+
+        private Transform playerTransform;
+        private Transform PlayerTransform => (playerTransform == null) ? playerTransform = transform : playerTransform;
+        
+
+        private void Awake()
+        {
+            Verts.Capacity = VERTS_PER_LOOP * (int)(segmentLength / distanceBetweenLoops);
+            Tris.Capacity = Verts.Capacity * 3;
+            UVs.Capacity = Verts.Capacity;
+
+            LoopVerts = CalculateLoopVertices();
+        }
+
+        private void Update()
+        {
+            if (CurrentSegment == null)
+                return;
+
+            if (Vector3.Distance(LastLoopPos, PlayerTransform.position) >= distanceBetweenLoops)
+                AddLoop();
+            PrevPos = PlayerTransform.position;
+        }
+
+        public void StartTube()
+        {
+            if (CurrentSegment != null)
+                return;
+
+            CurrentTubeParent = new GameObject();
+            PrevPos = PlayerTransform.position;
+            CreateEndCapSpheres(CurrentTubeParent);
+
+            
+            LastLoopPos = PlayerTransform.position;
+
+            
+            StartTubeSegment();
+        }
+
+        public void CloseTube()
+        {
+            if (CurrentSegment == null)
+                return;
+
+            AddLoop();
+            EndTubeSegment();
+            TubeFrontSphere.transform.SetParent(CurrentTubeParent.transform);
+            Tubes.Add(CurrentTubeParent);
+            CurrentTubeParent = null;
+        }
+
+        public void ClearTubes()
+        {
+            if (CurrentSegment != null)
+                CloseTube();
+
+            for (int i = 0; i < Tubes.Count; i++)
+                Destroy(Tubes[i]);
+        }
+
+        public void ClearTubeAtIndex(int index)
+        {
+            if(index == Tubes.Count)
+                CloseTube( );
+
+            Destroy(Tubes[index]);
+            Tubes.RemoveAt(index);
+        }
+
+        #region PRIVATES
+
+
+        private void CreateEndCapSpheres(GameObject currentTubeParent)
+        {
+            TubeFrontSphere = CreateSphere(PlayerTransform.position, tubeRadius, material);
+            TubeFrontSphere.transform.SetParent(PlayerTransform);
+            GameObject TubeBackSphere = CreateSphere(PlayerTransform.position, tubeRadius, material);
+            TubeBackSphere.transform.SetParent(currentTubeParent.transform);
+        }
+
+        private GameObject CreateSphere(Vector3 position, float radius, Material material)
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = new Vector3(radius * 2, radius * 2, radius * 2);
+            sphere.transform.position = position;
+            sphere.GetComponent<Renderer>().material = material;
+            Destroy(sphere.GetComponent<SphereCollider>());
+            return sphere;
+        }
+
+        private Vector4[] CalculateLoopVertices()
+        {
+            Vector4[] loop = new Vector4[VERTS_PER_LOOP];
+            float radians = (360 / (VERTS_PER_LOOP - 1)) * Mathf.Deg2Rad;
+            for (int i = 0; i < loop.Length; i++)
+                loop[i] = new Vector4(tubeRadius * Mathf.Sin(i * radians), tubeRadius * Mathf.Cos(i * radians), 0, 1);
+            return loop;
+        }
+
+        private void StartTubeSegment()
+        {
+            currentSegmentLength = 0;
+            Verts.Clear();
+            Tris.Clear();
+            UVs.Clear();
+          
+            CurrentSegment = new GameObject();
+            CurrentSegment.AddComponent<MeshFilter>();
+            CurrentSegment.AddComponent<MeshRenderer>();
+
+            Mesh = CurrentSegment.GetComponent<MeshFilter>().mesh;
+            Mesh.MarkDynamic();
+            CurrentSegment.GetComponent<Renderer>().material = material;
+            Mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+            UpdateVertsAndUVs();
+            Mesh.RecalculateNormals();
+            LoopCount = 1;
+        }
+
+        private void EndTubeSegment()
+        {
+            if (CurrentSegment == null)
+                return;
+            Mesh.Optimize();
+            CurrentSegment.transform.SetParent(CurrentTubeParent.transform);
+            CurrentSegment = null;
+        }
+
+        private void AddLoop()
+        {
+            currentSegmentLength += (PlayerTransform.position - LastLoopPos).magnitude;
+            LastLoopPos = PlayerTransform.position;
+
+            UpdateVertsAndUVs();
+            UpdateTriangles();
+            LoopCount++;
+            PrevVelocity = VelocityVector;
+
+            if (currentSegmentLength >= segmentLength)
+            {
+                EndTubeSegment();
+                StartTubeSegment();
+            }                       
+        }
+
+        private void UpdateVertsAndUVs()
+        {
+            Mesh.Clear();
+            for (int i = 0; i < VERTS_PER_LOOP; i++)
+            {
+                Verts.Add(LoopOrientMatrix * LoopVerts[i]);
+                UVs.Add(new Vector2((float)i / (VERTS_PER_LOOP - 1), Mathf.Clamp01(currentSegmentLength / segmentLength)));
+            }
+            Mesh.SetVertices(Verts);
+            Mesh.SetUVs(0, UVs);
+        }
+
+        private void UpdateTriangles()
+        {         
+            int vertInd = (LoopCount - 1) * VERTS_PER_LOOP;
+            for (int i = 0; i < VERTS_PER_LOOP - 1; i++)
+            {
+                Tris.Add(vertInd);
+                Tris.Add(vertInd + VERTS_PER_LOOP);
+                Tris.Add(vertInd + VERTS_PER_LOOP + 1);
+                Tris.Add(vertInd);
+                Tris.Add(vertInd + VERTS_PER_LOOP + 1);
+                Tris.Add(vertInd + 1);
+                vertInd++;
+            }
+
+            //seam triangles
+            Tris.Add(vertInd);
+            Tris.Add(vertInd + VERTS_PER_LOOP);
+            Tris.Add(vertInd + 1);
+            Tris.Add(vertInd);
+            Tris.Add(vertInd + 1);
+            Tris.Add(vertInd - VERTS_PER_LOOP + 1);
+
+            Mesh.SetTriangles(Tris, 0);
+            Mesh.RecalculateNormals();
+        }
+        #endregion
+    }
+}
